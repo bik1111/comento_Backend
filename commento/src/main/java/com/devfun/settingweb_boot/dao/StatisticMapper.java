146187502922dd75d3031a4ac3495a85dde013e2
Jin Cheol <76617139+bik1111@users.noreply.github.com>
2022-08-19 23:48:22 +0900

    Delete commento directory

--- commento/src/main/java/com/devfun/settingweb_boot/dao/StatisticMapper.java
+++
@@ -1,9 +0,0 @@
-package com.devfun.settingweb_boot.dao;
-import java.util.HashMap;
- 
-import com.devfun.settingweb_boot.dto.StatisticDto;
- 
-public interface  StatisticMapper {
-    public HashMap<String, Object> selectYearLogin(String year);
- 
-}
